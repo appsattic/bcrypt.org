b14caf1c991b41cb5386fc8d9fb02310b402ac9d
Andrew Chilton <andychilton@gmail.com>
2015-09-22 11:15:17 +1200

    Test usage of bcrypt

--- server.go
+++ server.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	password := "s3kr1t!"
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("hash=%v\n", string(hash))
 }
