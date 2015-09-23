0a6c525710e4329f0aef54a792f7611ebcbe9d71
Andrew Chilton <andychilton@gmail.com>
2015-09-23 23:28:28 +1200

    Add the front page, the API and the frontend UI for the test forms

--- server.go
+++ server.go
@@ -1,16 +1,134 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
+	"encoding/json"
+	"strconv"
+	"log"
 
-	// "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 	"github.com/gorilla/mux"
 )
 
+type GenerateHash struct {
+	Ok bool `json:"ok"`
+	Msg string `json:"msg"`
+	Password string `json:"password"`
+	Hash string `json:"hash"`
+	Cost int `json:"cost"`
+}
+
+type CheckPassword struct {
+	Ok bool `json:"ok"`
+	Msg string `json:"msg"`
+	Password string `json:"password"`
+	Hash string `json:"hash"`
+	Cost int `json:"cost"`
+}
+
+func checkErr(w http.ResponseWriter, err error) bool {
+	if err != nil {
+		log.Printf("err=%v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return true
+	}
+	return false
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	http.ServeFile(w, r, "static/" + r.URL.Path[1:])
+}
+
+func apiGenerateHash(w http.ResponseWriter, r *http.Request) {
+	// get the password and cost
+	password := r.FormValue("password")
+	costStr := r.FormValue("cost")
+
+	var err error
+
+	// convert cost to an integer
+	cost := 6
+	if len(costStr) == 0 {
+		cost = 6
+	} else {
+		cost, err = strconv.Atoi(costStr)
+		if checkErr(w, err) {
+			return
+		}
+	}
+
+	// use bcrypt to hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if checkErr(w, err) {
+		return
+	}
+
+	// create a datastructure to send back
+	data := GenerateHash{
+		true,
+		"",
+		password,
+		string(hash),
+		cost,
+	}
+
+	// send back some JSON
+	json, err := json.Marshal(data)
+	if checkErr(w, err) {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func apiCheckPassword(w http.ResponseWriter, r *http.Request) {
+	// get the password and cost
+	password := r.FormValue("password")
+	hash := r.FormValue("hash")
+
+	// ToDo: check this hash looks syntactically correct
+
+	data := CheckPassword{}
+
+	// check this password is or isn't correct
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err == nil {
+		// create a datastructure to send back
+		data.Ok = true
+		data.Password = password
+		data.Hash = string(hash)
+	} else {
+		// see if the password/hash were different or something else entirely
+		if err != bcrypt.ErrMismatchedHashAndPassword {
+			if checkErr(w, err) {
+				return
+			}
+		}
+
+		// create a datastructure to send back
+		data.Ok = false
+		data.Msg = "Incorrect password for this hash"
+		data.Password = password
+		data.Hash = string(hash)
+	}
+
+	// get the cost of this hash
+	cost, err := bcrypt.Cost([]byte(hash))
+	if checkErr(w, err) {
+		return
+	}
+	data.Cost = cost
+
+	// send back some JSON
+	json, err := json.Marshal(data)
+	if checkErr(w, err) {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
 func main() {
@@ -19,9 +137,12 @@ func main() {
 
 	// the handlers
 	router.HandleFunc("/", homeHandler)
-	router.Path("/robots.txt").Handler(http.FileServer(http.Dir("static")))
-	router.Path("/favicon.ico").Handler(http.FileServer(http.Dir("static")))
+	router.HandleFunc("/robots.txt", homeHandler)
+	router.HandleFunc("/favicon.ico", homeHandler)
 	router.PathPrefix("/s/").Handler(http.FileServer(http.Dir("static")))
+
+	router.HandleFunc("/api/generate-hash.json", apiGenerateHash)
+	router.HandleFunc("/api/check-password.json", apiCheckPassword)
 
 	// the server
 	http.Handle("/", router)
---
+++ static/s/js/ready.js
@@ -0,0 +1,55 @@
+$(function() {
+
+  $('#generate-hash').click(function() {
+    // get the params to send
+    var params = {
+      password : $('#password1').val(),
+      cost     : $('#cost1').val(),
+    };
+    var paramStr = $.param(params, true);
+
+    $.post('/api/generate-hash.json', paramStr, null, 'json')
+      .done(function(data) {
+        console.log('data:', data);
+        $('#hash1').text(data.hash);
+      })
+      .fail(function(err) {
+        console.warn('Something went wrong:', err);
+      })
+      .always(function() {
+        console.log('Done');
+      })
+    ;
+  });
+
+  $('#check-password').click(function() {
+    // get the params to send
+    var params = {
+      password : $('#password2').val(),
+      hash     : $('#hash2').val(),
+    };
+    var paramStr = $.param(params, true);
+
+    $.post('/api/check-password.json', paramStr, null, 'json')
+      .done(function(data) {
+        console.log('data:', data);
+        if ( data.ok ) {
+          $('#same').text('Yes');
+        }
+        else {
+          $('#same').text('No');
+        }
+        $('#cost2').text(data.cost);
+      })
+      .fail(function(err) {
+        console.warn('Something went wrong:', err);
+      })
+      .always(function() {
+        console.log('Done');
+      })
+    ;
+  });
+
+
+
+});
